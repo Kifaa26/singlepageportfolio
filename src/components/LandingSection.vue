1c8ebadc80e31124337852f13ca48520d9d2d68e
Kifaa26 <akifahpalekar@gmail.com>
2024-06-26 16:19:41 +0200

    LandingPage

---
+++ src/components/LandingSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img
+          src="https://github.com/Kifaa26/images/blob/main/images/1693394828293.jpg?raw=true"
+          alt="profile"
+          class="img-fluid"
+          loading="lazy"
+        />
+      </div>
+      <div class="col">
+        <h1 class="display-1">Aakifah Palekar</h1> 
+        <span>{{ jobTitle[0].title }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue'
+const jobTitle = 
+computed(() => this.$store.state.jobTitle)
+  onMounted(() => {
+    this.$store.dispatch('fetchJobTitle')
+  })
+</script>
+
+<style scoped>
+</style>
